8f508a2780c0dd3d1d20260a9ab8c3e5a458fad2
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-10-04 08:40:48 -0700

    game score

--- script.js
+++ script.js
@@ -92,6 +92,8 @@ window.addEventListener("load", function () {
       this.x = this.game.width;
       this.speedX = Math.random() * -1.5 - 0.05;
       this.markedForDeletion = false;
+      this.lives = 5;
+      this.score = this.lives;
     }
     update() {
       this.x += this.speedX;
@@ -100,6 +102,9 @@ window.addEventListener("load", function () {
     draw(context) {
       context.fillStyle = "red";
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.fillStyle = "black";
+      context.font = "20px Helvetica";
+      context.fillText(this.lives, this.x, this.y);
     }
   }
   class Angler1 extends Enemy {
@@ -154,6 +159,19 @@ window.addEventListener("load", function () {
       }
       this.enemies.forEach((enemy) => {
         enemy.update();
+        if (this.checkCollision(this.player, enemy)) {
+          enemy.markedForDeletion = true;
+        }
+        this.player.projectiles.forEach((projectile) => {
+          if (this.checkCollision(projectile, enemy)) {
+            enemy.lives--;
+            projectile.markedForDeletion = true;
+            if (enemy.lives <= 0) {
+              enemy.markedForDeletion = true;
+              this.score += enemy.score;
+            }
+          }
+        });
       });
       this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
       if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
@@ -172,6 +190,14 @@ window.addEventListener("load", function () {
     }
     addEnemy() {
       this.enemies.push(new Angler1(this));
+    }
+    checkCollision(rect1, rect2) {
+      return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.height + rect2.y > rect2.y
+      );
     }
   }
   const game = new Game(canvas.width, canvas.height);
